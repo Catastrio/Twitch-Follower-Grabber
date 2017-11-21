0c44935a5514b4bf4f8638d954be91b080ca4c6e
Catastrio <catastrio@gmail.com>
2017-11-21 16:12:00 -0700

    Open .ini with resources, key into .ini and fixing up variables

--- TwitchFollowerGrabber.py
+++ TwitchFollowerGrabber.py
@@ -1,25 +1,35 @@
-import requests, json, time
+import requests, json, time, configparser, string
 from time import sleep
 
 def main():
-
+    
+    config = configparser.ConfigParser()
     listOfUserLogins = []
     listOfUserIDs = []
     followerCounter = 0
     twitchIDCounter = 0
+    
+
     MY_OUTPUT_FILE = open("ListOfFollowers.txt", "w")
-    CIDFILE = open("config.ini", "r")
-    HEADERS = {'Client-ID': CIDFILE.read()}
     SECONDSTOSLEEP = 2
+    DATA_INDEX = 0
+    CIDFILE = "config.ini"
+
+    with open(CIDFILE) as f_obj:
+        config.read(CIDFILE)
+        HEADERS = {'Client-ID': config['DEFAULT']['Client_id']}
     
     print("To test to see if this program works, try following me on Twitch.tv/Catastrio and then using 'Catastrio' as a test!\n")
     userLoginName = input("What user would you like to grab the followers of?: ")
     userLoginEndpoint = 'https://api.twitch.tv/helix/users?login=' + userLoginName
 
     userLoginResponse = requests.get(url=userLoginEndpoint, data={}, headers=HEADERS).json()
-    userLoginNameId = userLoginResponse['data'][0]['id']
+    data = userLoginResponse['data'][DATA_INDEX]
+    user_id = data['id']
+    
+    #userLoginNameId = userLoginResponse['data'][DATA_INDEX]['id']
 
-    userFollowersEndpoint = 'https://api.twitch.tv/helix/users/follows?to_id=' + userLoginNameId + '&first=100'
+    userFollowersEndpoint = 'https://api.twitch.tv/helix/users/follows?to_id=' + user_id + '&first=100'
     userFollowersResponse = requests.get(url=userFollowersEndpoint, data={}, headers=HEADERS).json()
 
     followersOnPage = len(userFollowersResponse['data'])
@@ -31,7 +41,7 @@ def main():
     for twitchID in listOfUserIDs:
         userIdEndpoint = 'https://api.twitch.tv/helix/users?id=' + twitchID
         userIdResponse = requests.get(url=userIdEndpoint, data={}, headers=HEADERS).json()
-        listOfUserLogins = listOfUserLogins + [userIdResponse['data'][0]['login']]
+        listOfUserLogins = listOfUserLogins + [userIdResponse['data'][DATA_INDEX]['login']]
         twitchIDCounter += 1
         print(str(twitchIDCounter) + " out of " + str(len(listOfUserIDs)))
         time.sleep(SECONDSTOSLEEP)
