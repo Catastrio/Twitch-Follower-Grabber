8214b902bb27d8d50cfbe46a7d26ef39227e9fca
Catastrio <catastrio@gmail.com>
2017-11-23 11:29:07 -0700

    Consistent Variable Names
    Changed all variable names to snake case

--- TwitchFollowerGrabber.py
+++ TwitchFollowerGrabber.py
@@ -4,47 +4,48 @@ from time import sleep
 def main():
     
     config = configparser.ConfigParser()
-    listOfUserLogins = []
-    listOfUserIDs = []
-    followerCounter = 0
-    twitchIDCounter = 0
+    list_of_user_logins = []
+    list_of_usser_ids = []
+    follower_counter = 0
+    twitch_id_counter = 0
     
     MY_OUTPUT_FILE = open("ListOfFollowers.txt", "w")
     SECONDS_TO_SLEEP = 2
     DATA_INDEX = 0
-    CONFIGFILE = "config.ini"
+    CONFIG_FILE = "config.ini"
+    CLIENT_ID = 'Client_id'
 
-    with open(CONFIGFILE) as f_obj:
-        config.read(CONFIGFILE)
-        HEADERS = {'Client-ID': config['DEFAULT']['Client_id']}
+    with open(CONFIG_FILE) as f_obj:
+        config.read(CONFIG_FILE)
+        HEADERS = {'Client-ID': config['DEFAULT'][CLIENT_ID]}
     
     print("To test to see if this program works, try following me on Twitch.tv/Catastrio and then using 'Catastrio' as a test!\n")
-    twitchName = input("What user would you like to grab the followers of?: ")
-    userEndpoint = 'https://api.twitch.tv/helix/users?login=' + twitchName
+    twitch_name = input("What user would you like to grab the followers of?: ")
+    user_endpoint = 'https://api.twitch.tv/helix/users?login=' + twitch_name
 
-    userResponse = requests.get(url=userEndpoint, data={}, headers=HEADERS).json()
-    data = userResponse['data'][DATA_INDEX]
+    user_response = requests.get(url=user_endpoint, data={}, headers=HEADERS).json()
+    data = user_response['data'][DATA_INDEX]
     user_id = data['id']
 
-    userFollowsEndpoint = 'https://api.twitch.tv/helix/users/follows?to_id=' + user_id + '&first=100'
-    userFollowsResponse = requests.get(url=userFollowsEndpoint, data={}, headers=HEADERS).json()
+    user_follows_endpoint = 'https://api.twitch.tv/helix/users/follows?to_id=' + user_id + '&first=100'
+    user_follows_response = requests.get(url=user_follows_endpoint, data={}, headers=HEADERS).json()
 
-    followersOnPage = len(userFollowsResponse['data'])
-    print("Followers on page: " + str(followersOnPage))
+    followers_on_page = len(user_follows_response['data'])
+    print("Followers on page: " + str(followers_on_page))
 
-    listOfUserIDs = [ID['from_id'] for ID in userFollowsResponse['data']]
+    list_of_user_ids = [ID['from_id'] for ID in user_follows_response['data']]
 
     print("This process will take some time depending on how many followers a user has...")
-    for twitchID in listOfUserIDs:
-        userIdEndpoint = 'https://api.twitch.tv/helix/users?id=' + twitchID
-        userIdResponse = requests.get(url=userIdEndpoint, data={}, headers=HEADERS).json()
-        listOfUserLogins = listOfUserLogins + [userIdResponse['data'][DATA_INDEX]['login']]
-        twitchIDCounter += 1
-        print(str(twitchIDCounter) + " out of " + str(len(listOfUserIDs)))
-        time.sleep(SECONDSTOSLEEP)
+    for twitch_id in list_of_user_ids:
+        user_id_endpoint = 'https://api.twitch.tv/helix/users?id=' + twitch_id
+        user_id_response = requests.get(url=user_id_endpoint, data={}, headers=HEADERS).json()
+        list_of_user_logins = list_of_user_logins + [user_id_response['data'][DATA_INDEX]['login']]
+        twitch_id_counter += 1
+        print(str(twitch_id_counter) + " out of " + str(len(list_of_user_ids)))
+        time.sleep(SECONDS_TO_SLEEP)
 
-    for loginName in listOfUserLogins:
-        MY_OUTPUT_FILE.write(str(loginName) + "\n")
+    for login_name in list_of_user_logins:
+        MY_OUTPUT_FILE.write(str(login_name) + "\n")
 
     print("All done! Your list contains " + str(twitchIDCounter) + " followers!")
 
